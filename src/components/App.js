0d7ec27fabff933c1ca5097c919da4198e155d1f
phillipashe <ashephillip@gmail.com>
2022-06-07 18:58:17 -0400

    now persisting across refreshes

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,31 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
 
   const addContactHandler = (contact) => {
     setContacts([...contacts, contact]);
   };
 
+  useEffect(() => {
+    const storedContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storedContacts && storedContacts.length) {
+      setContacts(storedContacts);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
-    <div>
+    <div className="ui container">
       <Header />
       <AddContact addContactHandler={addContactHandler}/>
       <ContactList contacts={contacts} />
