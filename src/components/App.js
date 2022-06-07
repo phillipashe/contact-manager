4a6033469c8320d7906b5396657ff625ed629660
phillipashe <ashephillip@gmail.com>
2022-06-07 17:11:02 -0400

    Filled out my components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import React from 'react';
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+
 
 function App() {
   return (
-    <div>Cleared out react app defaults</div>
+    <div>
+      <Header />
+      <AddContact />
+      <ContactList />
+    </div>
   );
 }
 
