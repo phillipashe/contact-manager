974ba0e0769647294c51ae982a5468cbae01293c
phillipashe <ashephillip@gmail.com>
2022-06-10 18:13:10 -0400

    converted add contact to functional component

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,50 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class AddContact extends React.Component {
-  state = {
-    name: "",
-    email: ""
-  }
+// class AddContact extends React.Component {
+const AddContact = (props) => {
+  // state = {
+  //   name: "",
+  //   email: ""
+  // }
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
   // need to use arrow function here for this to refer to class
   // for regular functions, method invocation binds this to function
   // whereas simple invocation binds this to global object
   // arrow functions do not define their own execution context - no change to this
-  add = (e) => {
+  const add = (e) => {
     // prevents page refresh
     e.preventDefault();
-    if (!this.isValidEmail()) {
+    if (!isValidEmail()) {
       alert("Please enter a valid email.")
       return;
     }
-    if (!this.state.name || !this.state.email) {
+    if (!name || !email) {
       alert("Please fill out all the fields.")
       return;
     }
-    this.props.addContactHandler(this.state);
-    this.setState({ name: "", email: "" });
+    props.addContactHandler({name, email});
+    setName("");
+    setEmail("");
   }
 
-  isValidEmail() {
-    return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.state.email));
+  const isValidEmail = () => {
+    return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email));
   }
 
-  render() {
-    return (<div className="ui main">
-      <h2>Add Contact</h2>
-      <form className="ui form" onSubmit={this.add}>
-        <div className="field">
-          <label>Name</label>
-          <input type="text" name="name" placeholder="name" value={this.state.name} onChange={(e) => this.setState({ name: e.target.value })}></input>
-        </div>
-        <div className="field">
-          <label>Email</label>
-          <input type="text" name="email" placeholder="Email" value={this.state.email} onChange={(e) => this.setState({ email: e.target.value })}></input>
-        </div>
-        <button className="ui button blue">Add</button>
-      </form>
-    </div >)
-  }
+  return (<div className="ui main">
+    <h2>Add Contact</h2>
+    <form className="ui form" onSubmit={add}>
+      <div className="field">
+        <label>Name</label>
+        <input type="text" name="name" placeholder="name" value={name} onChange={(e) => setName(e.target.value)}></input>
+      </div>
+      <div className="field">
+        <label>Email</label>
+        <input type="text" name="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+      </div>
+      <button className="ui button blue">Add</button>
+    </form>
+  </div >)
 }
 
 export default AddContact;
