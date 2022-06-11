1f92bea6749ac8e14654f3107a3cbac4c91975a8
phillipashe <ashephillip@gmail.com>
2022-06-11 16:22:33 -0400

    Implement link for details page

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import user from "../images/user.png";
 import './ContactCard.css';
 
@@ -6,11 +7,15 @@ const ContactCard = (props) => {
   const { key, name, email } = props.contact;
   return (
     <div className="item">
-      <img className="ui avatar image" src={user} alt="user" />
-      <div className="content">
-        <div className="key">{key}</div>
-        <div className="header">{name}</div>
-        <div className="email">{email}</div>
+      <div className="details">
+        <Link to={`/details/${key}`}>
+          <img className="ui avatar image" src={user} alt="user" />
+          <div className="content">
+            <div className="key">{key}</div>
+            <div className="header">{name}</div>
+            <div className="email">{email}</div>
+          </div>
+        </Link>
       </div>
       <i onClick={() => props.deleteHandler(key)} className="trash icon"></i>
     </div>
