cbd93b8592f7f31a0dc7bb76e177247a53080d35
phillipashe <ashephillip@gmail.com>
2022-06-13 20:36:24 -0400

    added a button to go back

--- src/components/ContactDetails.js
+++ src/components/ContactDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import './ContactDetails.css';
+import { Link } from 'react-router-dom';
 
 const ContactDetails = () => {
   const location = new useLocation();
@@ -15,6 +16,10 @@ const ContactDetails = () => {
         <div className="header">{name}</div>
         <div className="description">{email}</div>
       </div>
+      <Link className="home-button" to="/">
+        <button className="ui primary button centered">Take me back
+      </button>
+      </Link>
     </div>
   );
 
