00138279e16053617fe65be8bdb2df244ee8777f
phillipashe <ashephillip@gmail.com>
2022-06-13 21:32:18 -0400

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# Contact Manager 
-
-## React app personal project to try routes, states, & functional components (among other things).
-
-Add contacts to a list, view them, and delete them.
---
+++ precache-manifest.aec7a030f5579fdc6010761490aef7d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea9a80017cf36a0195f6",
+    "url": "/contact-manager/static/css/main.d827540e.chunk.css"
+  },
+  {
+    "revision": "ea9a80017cf36a0195f6",
+    "url": "/contact-manager/static/js/main.ea9a8001.chunk.js"
+  },
+  {
+    "revision": "c7297e6c8016bf9a2714",
+    "url": "/contact-manager/static/js/1.c7297e6c.chunk.js"
+  },
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "1a169ee0e11d6f85260b7864aa916f2c",
+    "url": "/contact-manager/static/media/user.1a169ee0.png"
+  },
+  {
+    "revision": "60f327b0ea59821502cec15ba38180c5",
+    "url": "/contact-manager/index.html"
+  }
+];
